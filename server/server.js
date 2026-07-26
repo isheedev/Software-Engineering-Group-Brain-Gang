f1cf9f0dfcb28d25f5f226533756f30676c7d9ae
Alexander Khauv <4changt@gmail.com>
2026-07-25 23:32:03 -0400

    Simplify server.js to just start the extracted app

--- server/server.js
+++ server/server.js
@@ -1,20 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const app = express();
-
-const authRoutes = require('./routes/auth.routes');
-const projectRoutes = require('./routes/project.routes');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, '../client')));
-app.use('/uploads', express.static('uploads'));
-
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
-
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+const app = require('./app');
+ 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
