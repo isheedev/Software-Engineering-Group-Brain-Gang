3d5cf43d22188ec0dee5e52225eba251a3bf435e
Alexander Khauv <4changt@gmail.com>
2026-07-25 23:31:25 -0400

    Extract Express app setup into app.js for testability

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const authRoutes = require('./routes/auth.routes');
+const projectRoutes = require('./routes/project.routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+
+module.exports = app;
